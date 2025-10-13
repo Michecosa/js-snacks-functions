1857412265ba231d7571c36839342cc139b95012
Michela <micheladellagatta1@gmail.com>
2025-10-13 13:01:48 +0200

    Add function to count vocals

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,7 +4,16 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function countVocals(string) {
+  let vocals = 0;
+  for (let i = 0; i < string.length; i++) {
+    const letter = string[i].toLowerCase();
+    if(letter === 'a' || letter === 'e' ||letter === 'i' ||letter === 'o' ||letter === 'u') {
+      vocals++;
+    }
+  }
+  return vocals;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
